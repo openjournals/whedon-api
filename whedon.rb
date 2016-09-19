71dbb239638e387ead3cdf5fe9637e3eca8c68b0
Arfon Smith <arfon.smith@gmail.com>
2016-09-19 15:31:48 -0700

    More debug

--- whedon.rb
+++ whedon.rb
@@ -3,12 +3,12 @@ require 'sinatra'
 
 before do
   params = JSON.parse(request.env["rack.input"].read)
-  puts params.inspect
-  @message = params['body']
+  # Only work with issues
+  halt if params['issue'].nil?
+  @message = params['issue']['body']
 end
 
 post '/dispatch' do
-  puts @message
   case @message
   when /commands/i
     puts "I have all the commands"
