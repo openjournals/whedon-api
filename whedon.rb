c8d335813686f881161a3fccdbdd262767f23162
Arfon Smith <arfon.smith@gmail.com>
2016-09-19 15:38:16 -0700

    Always debug the payload

--- whedon.rb
+++ whedon.rb
@@ -1,9 +1,12 @@
 require 'json'
 require 'sinatra'
 
+# Before we handle the request we extract the issue body to grab the whedon
+# command (if present).
 before do
   params = JSON.parse(request.env["rack.input"].read)
-  # Only work with issues
+  # Only work with issues. Halt if there isn't an issue in the JSON
+  puts "DEBUG: #{params}"
   halt if params['issue'].nil?
   @message = params['issue']['body']
 end
