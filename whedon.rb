1d1aacf7f41b6f57149797141e49eff091227e44
Arfon Smith <arfon.smith@gmail.com>
2016-09-22 08:21:03 -0700

    Typo fix

--- whedon.rb
+++ whedon.rb
@@ -142,7 +142,7 @@ def start_review
   url = "http://joss.theoj.org/papers/api_start_review?id=#{@issue_id}&editor=#{editor}&reviewer=#{reviewer}&secret=#{settings.joss_api_key}"
   # TODO let's do some error handling here please
   puts "POSTING TO #{url}"
-  respose = RestClient.post(url, "")
+  response = RestClient.post(url, "")
   paper = JSON.parse(response)
   return paper['review_issue_id']
 end
