c4fe54fde13b9075761fdca1fbba88c3ee3d34a6
Arfon Smith <arfon.smith@gmail.com>
2017-11-12 12:14:01 -0500

    Add Heroku-specific configs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,24 @@
-# set path to app that will be used to configure unicorn,
-# note the trailing slash in this example
-@dir = "/home/deployer/whedon/current/"
-
-worker_processes 2
-working_directory @dir
-
+worker_processes 3
 timeout 30
+preload_app true
 
-# Specify path to socket unicorn listens to,
-# we will use this in our nginx.conf later
-listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+before_fork do |server, worker|
 
-# Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
 
-# Set log file paths
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
