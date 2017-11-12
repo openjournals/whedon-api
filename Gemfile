bebbcd430144048cd30746006a15a28992b04ce7
Arfon Smith <arfon.smith@gmail.com>
2017-11-12 16:32:49 -0500

    Removing SideKiq

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source "https://rubygems.org"
 
 gem "octokit", "4.7.0"
 gem "rest-client", "2.0.2"
-gem "sidekiq", "5.0.5"
 gem "sinatra", "2.0.0"
 gem "sinatra-contrib", "2.0.0"
 gem "unicorn", "5.3.1"
--- whedon.rb
+++ whedon.rb
@@ -2,7 +2,6 @@ require 'yaml'
 require 'json'
 require 'octokit'
 require 'rest-client'
-require 'sidekiq'
 require 'sinatra'
 
 set :views, Proc.new { File.join(root, "responses") }
@@ -109,19 +108,12 @@ def robawt_respond
   when /\A@whedon assignments/i
     reviewers, editors = assignments
     respond erb :assignments, :locals => { :reviewers => reviewers, :editors => editors, :all_editors => @config.editors }
-  when /\A@whedon generate pdf/i
-    # TO-DO: Pass to sidekiq/the whedon gem
   end
 end
 
 # How Whedon talks
 def respond(comment)
   settings.github.add_comment(@nwo, @issue_id, comment)
-end
-
-# Download and compile the PDF
-def process_pdf
-  WhedonWorker.perform_async(@nwo)
 end
 
 def assign_archive(doi_string)
@@ -217,22 +209,5 @@ def check_editor
   unless @config.editors.include?(@sender)
     respond "I'm sorry @#{@sender}, I'm afraid I can't do that. That's something only editors are allowed to do."
     halt 403
-  end
-end
-
-class WhedonWorker
-  include Sidekiq::Worker
-
-  def perform(repository)
-    download(repository)
-    compile(repository)
-  end
-
-  def download(repository)
-    puts "Downloading #{repository}"
-  end
-
-  def compile(repository)
-    puts "Compiling #{repository}"
   end
 end
