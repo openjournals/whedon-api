ceee5dba6515f9b2c1126b2be4906e29643fb518
Arfon Smith <arfon.smith@gmail.com>
2017-11-12 21:40:45 -0500

    Debug

--- whedon.rb
+++ whedon.rb
@@ -112,7 +112,7 @@ def robawt_respond
     respond erb :assignments, :locals => { :reviewers => reviewers, :editors => editors, :all_editors => @config.editors }
   when /\A@whedon generate pdf/i
     puts "Attempting to compile PDF"
-    respond "```#{process_pdf}```"
+    respond "```\n#{process_pdf}\n```"
   end
 end
 
