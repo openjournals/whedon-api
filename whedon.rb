32e50f058dd3827f287ad8a740b7711bf0c5a81a
Arfon Smith <arfon.smith@gmail.com>
2017-12-06 16:57:44 -0500

    Debug

--- whedon.rb
+++ whedon.rb
@@ -100,6 +100,9 @@ end
 def say_goodbye
   if issue.title.match(/^\[REVIEW\]:/)
     # If the REVIEW has been marked as 'accepted'
+    puts "ISSUE LABELS #{issue.labels}"
+    puts issue.labels.select {|l| l.name }
+    
     if issue.labels.select {|l| l.name }.include?('accepted')
       respond erb :goodbye, :locals => {:site_host => @config.site_host,
                                         :doi_prefix => @config.doi_prefix,
