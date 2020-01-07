2c7c8300a7e93abeef6109c1c53352fe27bfbb60
Daniel S. Katz <d.katz@ieee.org>
2020-01-07 16:47:42 -0600

    fix assign first reviewer wording (#60)
    "now the reviewer" implied only one was needed. This is more general.

--- whedon_api.rb
+++ whedon_api.rb
@@ -132,7 +132,7 @@ class WhedonApi < Sinatra::Base
     when /\A@whedon assign (.*) as reviewer/i
       check_editor
       assign_reviewer($1)
-      respond "OK, the reviewer is #{$1}"
+      respond "OK, #{$1} is now a reviewer"
     when /\A@whedon add (.*) as reviewer/i
       check_editor
       add_reviewer($1)
