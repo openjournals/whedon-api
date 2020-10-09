63aae4bb185721130fbfeafb3028e11c9dc5c4a7
Arfon Smith <arfon@github.com>
2020-10-09 11:01:25 +0100

    Fixing rogue tmp

--- workers.rb
+++ workers.rb
@@ -144,7 +144,7 @@ class DOIWorker
 
     if status.success?
       # Need to checkout the new branch before looking for the paper.
-      `cd tmp/#{issue_id} && git checkout #{custom_branch} --quiet && cd` if custom_branch
+      `cd #{jid} && git checkout #{custom_branch} --quiet && cd` if custom_branch
 
       paper_path = find_paper(issue_id, jid)
       
