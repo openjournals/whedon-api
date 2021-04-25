2e1707d8e7751b10f1d7bb1d4f6dbd44997a3ea8
Agah Karakuzu <agahkarakuzu@gmail.com>
2021-04-25 18:50:29 -0400

    Fix pandoc-citeproc

--- workers.rb
+++ workers.rb
@@ -30,7 +30,7 @@ class PaperPreviewWorker
     journal_name = "Journal of Open Source Software"
 
     if paper_paths.empty?
-      self.payload = "Can't find any papers to compile. Make sure there's a file named <code>paper.md</code> in your repository."
+      self.payload = "Can't find any papers to compile. Make sure there's a file named <code>preprint.md</code> in your repository."
       abort("Can't find any papers to compile.")
     elsif paper_paths.size == 1
       begin
@@ -46,7 +46,7 @@ class PaperPreviewWorker
       directory = File.dirname(paper_paths.first)
       # TODO: may eventually want to swap out the latex template
 
-      result, stderr, status = Open3.capture3("cd #{directory} && pandoc -V repository='#{repository_address}' -V archive_doi='PENDING' -V paper_url='PENDING' -V journal_name='#{journal_name}' -V formatted_doi='10.21105/#{journal}.0XXXX' -V review_issue_url='XXXX' -V graphics='true' -V issue='X' -V volume='X' -V page='X' -V logo_path='#{Whedon.resources}/#{journal}/logo.png' -V aas_logo_path='#{Whedon.resources}/#{journal}/aas-logo.png' -V year='XXXX' -V submitted='01 January XXXX' -V published='01 January XXXX' -V editor_name='Editor Name' -V editor_url='http://example.com' -V citation_author='Mickey Mouse et al.' -o #{sha}.pdf -V geometry:margin=1in --pdf-engine=xelatex --filter pandoc-citeproc #{File.basename(paper_paths.first)} --from markdown+autolink_bare_uris --csl=#{csl_file} --template #{latex_template_path}")
+      result, stderr, status = Open3.capture3("cd #{directory} && pandoc -V repository='#{repository_address}' -V archive_doi='PENDING' -V paper_url='PENDING' -V journal_name='#{journal_name}' -V formatted_doi='10.21105/#{journal}.0XXXX' -V review_issue_url='XXXX' -V graphics='true' -V issue='X' -V volume='X' -V page='X' -V logo_path='#{Whedon.resources}/#{journal}/logo.png' -V aas_logo_path='#{Whedon.resources}/#{journal}/aas-logo.png' -V year='XXXX' -V submitted='01 January XXXX' -V published='01 January XXXX' -V editor_name='Editor Name' -V editor_url='http://example.com' -V citation_author='Mickey Mouse et al.' -o #{sha}.pdf -V geometry:margin=1in --pdf-engine=xelatex --citeproc #{File.basename(paper_paths.first)} --from markdown+autolink_bare_uris --csl=#{csl_file} --template #{latex_template_path}")
 
       if status.success?
         if File.exists?("#{directory}/#{sha}.pdf")
@@ -58,8 +58,8 @@ class PaperPreviewWorker
         abort("Looks like we failed to compile the PDF.")
       end
     else
-      self.payload = "There seems to be more than one paper.md present. Aborting..."
-      abort("There seems to be more than one paper.md present. Aborting...")
+      self.payload = "There seems to be more than one preprint.md present. Aborting..."
+      abort("There seems to be more than one preprint.md present. Aborting...")
     end
   end
 
@@ -418,7 +418,7 @@ class DOIWorker
     paper_paths = []
 
     Find.find(search_path) do |path|
-      paper_paths << path if path =~ /paper\.tex$|paper\.md$/
+      paper_paths << path if path =~ /preprint\.tex$|preprint\.md$/
     end
 
     return paper_paths.first
